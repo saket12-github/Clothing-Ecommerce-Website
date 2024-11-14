788a0bbceeb0401a4401c309d73cf1aa980c16f2
saket12-github <saketchaudhari12@gmail.com>
2024-11-14 14:23:11 +0530

    Initial commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./DescriptionBox.css";
+
+export const DescriptionBox = () => {
+  return (
+    <div className="descriptionBox">
+      <div className="descriptionBox-navigator">
+        <div className="descriptionBox-nav-box">Description</div>
+        <div className="descriptionBox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionBox-description">
+        <p>
+          Made from ultra-soft, high-quality cotton blend, this sweatshirt
+          ensures that your child stays warm and comfortable throughout the day.
+          The fabric is gentle on the skin, making it ideal for playtime,
+          school, or cozying up on chilly evenings.
+        </p>
+        <p>
+          Made from ultra-soft, high-quality cotton blend, this sweatshirt
+          ensures that your child stays warm and comfortable throughout the day.
+          The fabric is gentle on the skin, making it ideal for playtime,
+          school, or cozying up on chilly evenings.
+        </p>
+      </div>
+    </div>
+  )
+}
